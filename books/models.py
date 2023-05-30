c9e14112d5ac38888dcd726affb3d0b9d7354640
zhuk-zhenya <zhuk-zhenya1@yandex.ru>
2023-05-30 18:42:36 +0300

    relationships between genres and books

--- books/models.py
+++ books/models.py
@@ -7,6 +7,9 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     year = models.IntegerField()
+    raiting = models.IntegerField(default=0)
+    publisher = models.CharField(max_length=50, null=True)
+    genre = models.ForeignKey("Genre", on_delete=models.DO_NOTHING, null=True, blank=True,related_name="books")
 
     def __str__(self):
         return f"Book title: {self.title} Author: {self.author}"
@@ -14,3 +17,6 @@ class Book(models.Model):
 
 class Genre(models.Model):
     title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Genre: {self.id}, {self.title}"
