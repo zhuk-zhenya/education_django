d98e89ec722753a1589c37b6e56237830fa67104
zhuk-zhenya <zhuk-zhenya1@yandex.ru>
2023-05-30 10:05:37 +0300

    first commit

---
+++ books/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f"Book title: {self.title} Author: {self.author}"
